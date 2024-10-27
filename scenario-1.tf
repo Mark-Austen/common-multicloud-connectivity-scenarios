c85ed8a35c63bf5eca95415253aa23ecb3bf0bd7
Mark Austen <117334224+Mark-Austen@users.noreply.github.com>
2024-10-28 09:45:23 +1100

    Create scenario-1.tf

---
+++ scenario-1.tf
@@ -0,0 +1,96 @@
+data "megaport_location" "location_1" {
+  name = "Equinix SG1"
+}
+
+resource "megaport_port" "port_1_sin" {
+  product_name           = "Port 1 SIN"
+  port_speed             = 10000
+  location_id            = data.megaport_location.location_1.id
+  contract_term_months   = 1
+  marketplace_visibility = false
+  diversity_zone         = "red"
+}
+
+data "megaport_location" "location_2" {
+  name = "Equinix SG2"
+}
+
+data "megaport_partner" "aws_port_1_sin" {
+  connect_type = "AWSHC"
+  company_name = "AWS"
+  product_name = "Asia Pacific (Singapore) (ap-southeast-1)"
+  location_id  = data.megaport_location.location_2.id
+}
+
+resource "megaport_vxc" "aws_vxc_1_sin" {
+  product_name         = "AWS VXC - Primary"
+  rate_limit           = 50
+  contract_term_months = 1
+
+  a_end = {
+    requested_product_uid = megaport_port.port_1_sin.product_uid
+    ordered_vlan          = 301
+  }
+
+  b_end = {
+    requested_product_uid = data.megaport_partner.aws_port_1_sin.product_uid
+  }
+
+  b_end_partner_config = {
+    partner = "aws"
+    aws_config = {
+      name           = "AWS VXC - Primary"
+      type           = "private"
+      connect_type   = "AWSHC"
+      owner_account  = "<aws account id>"
+      diversity_zone = "red"
+    }
+  }
+}
+
+resource "megaport_vxc" "azure_vxc_1_sin" {
+  product_name            = "Azure VXC - Primary"
+  rate_limit              = 50
+  contract_term_months    = 1
+
+  a_end = {
+    requested_product_uid = megaport_port.port_1_sin.product_uid
+  }
+
+  b_end = {}
+
+  b_end_partner_config = {
+    partner = "azure"
+    azure_config = {
+      port_choice = "primary"
+      service_key = "<expressroute service key>"
+        peers = [{
+        type             = "private"
+        vlan             = 401
+        peer_asn         = 65001
+        primary_subnet   = "192.168.100.0/30"
+        secondary_subnet = "192.168.100.4/30"
+      }]
+    }
+  }
+}
+
+resource "megaport_vxc" "gcp_vxc_1" {
+  product_name         = "Google Cloud VXC - Primary"
+  rate_limit           = 50
+  contract_term_months = 1
+
+  a_end = {
+    requested_product_uid = megaport_port.port_1_sin.product_uid
+    ordered_vlan          = 501
+  }
+
+  b_end = {}
+
+  b_end_partner_config = {
+    partner = "google"
+    google_config = {
+      pairing_key = "<goole partner interconnect pairing key"
+    }
+  }
+}
